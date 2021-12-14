5746bba0f03584c44a9e495712c6a16ef78707b6
Loc <hoanglocst900@gmail.com>
2021-12-14 16:50:09 +0700

    SETUP - backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+
+const Config = ConfigModule.forRoot({
+  isGlobal: true,
+  envFilePath: `./config/.env.` + process.env.NODE_ENV,
+});
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [Config, UserModule, AuthModule],
   providers: [AppService],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  const logger = new Logger('SERVER');
+
+  await app.listen(port, () => {
+    logger.log(`Listening on port ${port}`);
+    logger.log(`Current NODE_ENV: ${process.env.NODE_ENV}`);
+    logger.log(`Current debug status: ${process.env.DOC}`);
+    logger.log(`Cors allows access: ${process.env.CLIENT_URL}`);
+    logger.log(
+      `Database Information: ${process.env.DB_HOST} - ${process.env.DB_NAME}`,
+    );
+    logger.log('Ready for service');
+  });
 }
 bootstrap();
