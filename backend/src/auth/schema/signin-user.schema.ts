23d88251608e83014c9a3e410eaacea4b845497f
Loc <hoanglocst900@gmail.com>
2021-12-16 17:08:38 +0700

    UPDATE - new version joi message

--- backend/src/auth/schema/signin-user.schema.ts
+++ backend/src/auth/schema/signin-user.schema.ts
@@ -1,8 +1,23 @@
 import * as Joi from 'joi';
+import JoiMessage from 'joi-message';
+import { ResponseMessage } from 'src/utils/message/responseMessage.enum';
 
 export const signinUserSchema = Joi.object({
-  //
-  email: Joi.string().email().required(),
-  //
-  password: Joi.string().min(6).max(50).required().trim(),
+  email: Joi.string()
+    .email()
+    .required()
+    .messages({
+      ...JoiMessage.createStringMessages({ field: 'Email' }),
+      'string.email': ResponseMessage.SIGNIN_FAIL,
+    }),
+  password: Joi.string()
+    .min(6)
+    .max(50)
+    .required()
+    .trim()
+    .messages({
+      ...JoiMessage.createStringMessages({ field: 'Password' }),
+      'string.min': ResponseMessage.SIGNIN_FAIL,
+      'string.max': ResponseMessage.SIGNIN_FAIL,
+    }),
 });
