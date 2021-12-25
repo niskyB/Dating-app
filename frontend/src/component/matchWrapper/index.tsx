29f241e4f5e1b9b6946a101b3f37bcb5bb0cba60
Duc Dau <dauleduc2@gmail.com>
2021-12-26 03:32:51 +0700

    UPDATE - shadow on card

--- frontend/src/component/matchWrapper/index.tsx
+++ frontend/src/component/matchWrapper/index.tsx
@@ -5,7 +5,7 @@ const MatchWrapper: React.FunctionComponent<MatchWrapperProps> = ({
 }) => {
   return (
     <div className="relative flex  items-center justify-center w-full h-screen text-center overflow-scroll">
-      <div className="relative flex flex-col bg-black w-96 h-168 shadow-lg">
+      <div className="relative flex flex-col bg-black w-96 h-168 shadow-2xl">
         {children}
       </div>
     </div>
