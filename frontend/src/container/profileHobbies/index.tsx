6c35164f080ec148d0ad35401ba33e191e1de2e1
Duc Dau <dauleduc2@gmail.com>
2021-12-25 22:00:16 +0700

    UPDATE - set limit of hobbies show on card to 6

--- frontend/src/container/profileHobbies/index.tsx
+++ frontend/src/container/profileHobbies/index.tsx
@@ -58,13 +58,16 @@ const ProfileHobbies: React.FunctionComponent<ProfileHobbiesProps> = ({
         </div>
       </div>
       <div className="flex flex-wrap mt-3 border-gray-300 focus-within:border-indigo-600">
-        {data.map((hobby) => {
-          return (
-            <Badge
-              value={hobby.hobbies}
-              onRemove={() => onRemoveHobby(hobby.id)}
-            />
-          );
+        {data.map((hobby, index) => {
+          if (index <= 6) {
+            return (
+              <Badge
+                value={hobby.hobbies}
+                onRemove={() => onRemoveHobby(hobby.id)}
+              />
+            );
+          }
+          return <></>;
         })}
       </div>
     </div>
