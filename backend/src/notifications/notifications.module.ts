d4407f4ce49f023b3c1454fd604ea017b846d28e
Duc Dau <dauleduc2@gmail.com>
2021-12-29 16:10:46 +0700

    ADD - socketio in server

--- backend/src/notifications/notifications.module.ts
+++ backend/src/notifications/notifications.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
 import { NotificationsGateway } from './notifications.gateway';
 import { NotificationsService } from './notifications.service';
 
 @Module({
+  imports: [AuthModule],
   providers: [NotificationsService, NotificationsGateway],
 })
 export class NotificationsModule {}
