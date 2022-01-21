3eaa24857f83b800051da6189cf97702cf8534d5
Duc Dau <dauleduc2@gmail.com>
2022-01-22 03:53:46 +0700

    FIX - chat load message socket

--- backend/src/chat/chat.actions.ts
+++ backend/src/chat/chat.actions.ts
@@ -3,6 +3,7 @@ export enum ChatAction {
   CHAT_RECEIVE = 'receive-message',
   CHAT_JOIN = 'join-message',
   CHAT_GET = 'get-message',
+  CHAT_GET_INIT_MESSAGE = 'get-init-message',
   CHAT_LEAVE = 'leave-chat',
   CHAT_SEEN_MESSAGE = 'seen-message',
   CHAT_UPDATE_CHAT_LIST = 'update-chatList',
